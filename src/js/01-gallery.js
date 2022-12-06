ab4fbcfdce94e2a0cb6fbfeb2b0f82bfaa2a9cea
Petro Psekho <psekho.pp@gmail.com>
2022-12-06 09:33:48 +0200

    solves task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox'; // Описаний в документації
+
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Додатковий імпорт стилів
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML(
+  'beforeend',
+  galleryItems
+    .map(
+      galleryItem =>
+        `<div class="gallery__item">
+        <a clss="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image"
+        src="${galleryItem.preview}" 
+        data-source="${galleryItem.original}"
+        alt= "${galleryItem.description}"></a></div>`
+    )
+    .join('')
+);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
