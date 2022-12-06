70b7f095d9099a5298c136c8afdbbf50a47889fc
Petro Psekho <psekho.pp@gmail.com>
2022-12-06 16:43:07 +0200

    task #2 in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Vimeo(iframe);
+
+player.on(
+  'play',
+  throttle(function setCurrentTime(e) {
+    console.log(e);
+    console.log(e.seconds);
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const video = document.querySelector('#vimeo-player');
+
+// const player = new Player(video);
+
+// player.on(
+//   'timeupdate',
+//   throttle(function playTime(e) {
+//     localStorage.setItem('vid-time', e.seconds);
+//   }, 1000)
+// );
+
+// player.setCurrentTime(localStorage.getItem('vid-time'));
