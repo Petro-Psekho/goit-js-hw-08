0439736f50a8e78db12c1a69c5800534b1a6c211
Petro Psekho <psekho.pp@gmail.com>
2022-12-06 19:17:25 +0200

    solves task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,15 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector('iframe');
 
 const player = new Vimeo(iframe);
 
 player.on(
-  'play',
-  throttle(function setCurrentTime(e) {
-    console.log(e);
-    console.log(e.seconds);
-    localStorage.setItem('videoplayer-current-time', e.seconds);
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
   }, 1000)
 );
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const video = document.querySelector('#vimeo-player');
-
-// const player = new Player(video);
-
-// player.on(
-//   'timeupdate',
-//   throttle(function playTime(e) {
-//     localStorage.setItem('vid-time', e.seconds);
-//   }, 1000)
-// );
-
-// player.setCurrentTime(localStorage.getItem('vid-time'));
