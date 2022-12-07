2730604bc6bf5f537b07cb323d28665dbc92a3ce
Petro Psekho <psekho.pp@gmail.com>
2022-12-07 16:49:02 +0200

    task #3 in progress

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  submitBtn: document.querySelector('.feedback-form'),
+};
+
+populateTextarea();
+
+refs.email.addEventListener('input', throttle(onEmailInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextariaInput, 500));
+refs.submitBtn.addEventListener('submit', onFormSubmit);
+
+function onEmailInput(e) {
+  const email = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, email);
+}
+
+function onTextariaInput(e) {
+  const message = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
